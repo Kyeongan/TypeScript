465f6c57dbaaef89285120f773aebdf40220b1a9
Karl Kwon <Karl.Kwon@EpeInnovations.local>
2016-08-25 16:07:08 -0500

    TypeScript annotations are great!

---
+++ grammar.js
@@ -0,0 +1,12 @@
+var x = 1;
+var y;
+var firstname = 'John';
+var lastname = "Papa";
+var num1 = 100;
+var num2 = 200;
+function addNum(n1, n2, n3) {
+    var result = n1 + n2 + n3;
+    var msg = "Sum is " + result;
+    alert(msg);
+}
+addNum(num1, num2, 6);
---
+++ grammar.ts
@@ -0,0 +1,14 @@
+var x =1;
+var y;
+var firstname = 'John';
+var lastname:string = "Papa";
+var num1 = 100;
+var num2 : number = 200;
+
+function addNum(n1:number, n2:number, n3:number) { 
+	var result = n1+n2+n3;
+	var msg = "Sum is " + result;
+	alert(msg);
+}
+
+addNum(num1, num2, 6);
