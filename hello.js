fe22880fe5b18cbe858a7ab97f1b13073d51325b
Karl Kwon <Karl.Kwon@EpeInnovations.local>
2016-08-25 15:35:53 -0500

    TypeScript hello

--- hello.js
+++ hello.js
@@ -1,4 +1,25 @@
 function greet(msg) {
     console.log(msg);
 }
+function greetingPeople(msg) {
+    console.log(msg + " Great!");
+}
 greet("Windows 10");
+greetingPeople("Karl!");
+var Car = (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    Car.prototype.start = function () {
+        alert("Engine Started: " + this.engine);
+    };
+    Car.prototype.stop = function () {
+        alert("Engine Stoped: " + this.engine);
+    };
+    return Car;
+}());
+window.onload = function () {
+    var car = new Car('V8');
+    car.start();
+    car.stop();
+};
--- hello.ts
+++ hello.ts
@@ -2,4 +2,32 @@ function greet(msg:string) {
 	console.log(msg);
 }
 
+function greetingPeople(msg:string) {
+	console.log(msg + " Great!");
+}
+
 greet("Windows 10");
+greetingPeople("Karl!");
+  
+
+class Car {
+	engine: string;
+
+	constructor(engine: string) {
+		this.engine = engine;
+	}
+
+	start() {
+		alert("Engine Started: " + this.engine);
+	}
+
+	stop() {
+		alert("Engine Stoped: " + this.engine);
+	}
+}
+
+window.onload = function (){
+	var car = new Car('V8');
+	car.start();
+	car.stop();
+}
